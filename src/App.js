8a1d46610ed0c5b5d707dfcf8d679e8d7b30aa3f
german <germanleon450@gmail.com>
2020-07-24 09:57:37 -0500

    mi publicacion 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,35 @@
 import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+/*import React from 'react';
 import Saludos from './evaluacion/puntos';
 import NumText from './evaluacion/numerotexto';
 import Matrices from './evaluacion/GoMatrices';
@@ -13,57 +44,6 @@ function App() {
       <hr />
       <Matrices />
 
-    </div>
-  );
-}
-
-export default App;
-
-
-
-
-
-/*import React from 'react';
-import MicomponenteDos from './components/Micomponente2';
-
-import Saludo from './components/01Saludo';
-import Contador from './components/02contador';
-import Operado from './components/03OperadorT';
-import Matriz from './components/04Matriz';
-import MatrizOperaciones from './components/05Matriz';
-
-function App(){
-  return (
-    <div>
-
-      <h1> Practicas:....React</h1>
-      <hr />
-      <MicomponenteDos />
-      <hr />
-      <Saludo variable=" Otro Saludo en Variable" />
-      <hr />
-      <Contador />
-      <hr />
-      <Operado />
-      <hr />
-     <Matriz />
-     <hr />
-     <MatrizOperaciones />
-    </div>
-  );
-}
-
-export default App;
-*/
-/*
-import React from 'react';
-import Contador from'./components/Contador' //con o sin extension js
-
-function App() {
-  return (
-    <div className="App">  
-       <h1>hola mundo desde react</h1>
-       <Contador /> 
     </div>
   );
 }
