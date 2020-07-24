0635141d592a2fb7fc517b6bd465c7d4da93b93e
german <germanleon450@gmail.com>
2020-07-24 09:06:28 -0500

    Updates

---
+++ precache-manifest.421c7a5cb20f6e3ab63ef7a75dc3ee71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "938072cc8f403f231bfcd8859014701a",
+    "url": "/my-app2-react/index.html"
+  },
+  {
+    "revision": "1d33b6ace5a015ba0144",
+    "url": "/my-app2-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "310653cd83a3266a5588",
+    "url": "/my-app2-react/static/js/2.3e33d232.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app2-react/static/js/2.3e33d232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d33b6ace5a015ba0144",
+    "url": "/my-app2-react/static/js/main.9bfb9fc5.chunk.js"
+  },
+  {
+    "revision": "07903cd5c750cd79c452",
+    "url": "/my-app2-react/static/js/runtime-main.2e107151.js"
+  }
+]);
