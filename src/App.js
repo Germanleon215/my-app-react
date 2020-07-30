041be81898cf7947e76e9f965b5ba8116cd7c5bb
german <germanleon450@gmail.com>
2020-07-30 00:16:35 -0500

    mi publicacion 2

--- src/App.js
+++ src/App.js
@@ -1,35 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-
-
-
-
-
-/*import React from 'react';
 import Saludos from './evaluacion/puntos';
 import NumText from './evaluacion/numerotexto';
 import Matrices from './evaluacion/GoMatrices';
@@ -48,4 +17,4 @@ function App() {
   );
 }
 
-export default App;*/
+export default App;
